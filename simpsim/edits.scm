1fc7f13f40852edce893840a8754e402d51e6f5b
= <matthew.peter.smith.599@gmail.com>
2025-05-25 14:02:48 +0100

    Edits have more consistent invocation

--- simpsim/edits.scm
+++ simpsim/edits.scm
@@ -1,8 +1,38 @@
 (define-module (simpsim edits))
   
 
-(define-public (c-style-param var)
+(define-public c-style-param
+  (lambda (var)
+	(lambda (val)
+	  (lambda (file)
+		(format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$/\\1~a/g\" \"~a\"~%"
+				var val file)))))
+
+(define-public (remove-param var)
   (lambda (val)
 	(lambda (file)
-	  (format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$/\\1~a/g\" \"~a\"~%"
+	  (format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$//g\" \"~a\"~%"
+			  var file))))
+
+(define-public (remove-when null-val)
+  (lambda (var)
+	(lambda (val)
+	  (lambda (file)
+		(if (equal? val null-val)
+			(((remove-param var) val) file)
+			"")))))
+				
+
+(define-public (insert-when-absent var)
+  (lambda (val)
+	(lambda (file)
+	  (format #f "grep \"^\\s*~a\\s*=\" \"~a\" || echo \"~a = ~a\" >> \"~a\"~%"
+			  var file
 			  var val file))))
+
+(define-public (edit-compose . fs)
+  (lambda (var)
+	(lambda (val)
+	  (lambda (file)
+		(string-concatenate (map (lambda (f) (((f var) val) file))
+								 fs))))))
