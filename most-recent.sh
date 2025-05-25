ecb728c95df91b6d49729ec733a8898b7fac2d05
= <matthew.peter.smith.599@gmail.com>
2025-05-25 09:49:16 +0100

    Changed Bash-specific syntax so more POSIX-compliant

--- most-recent.sh
+++ most-recent.sh
@@ -4,8 +4,8 @@ SECONDS=""
 RECENT=""
 
 while read dir ; do
-    [[ -f "$dir/meta/seconds" ]] && THIS=$(cat "$dir/meta/seconds") &&
-		([[ -z "$RECENT" ]] || [[ $THIS -ge $SECONDS ]]) && SECONDS=$THIS && RECENT=$dir
+    [ -f "$dir/meta/seconds" ] && THIS=$(cat "$dir/meta/seconds") &&
+		([ -z "$RECENT" ] || [ $THIS -ge $SECONDS ]) && SECONDS=$THIS && RECENT=$dir
 done
 
 echo $RECENT
