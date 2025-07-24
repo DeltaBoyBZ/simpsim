05832ea6c4b0a09c619a2422500495f3fa9f66a9
Matthew Smith <matthew.peter.smith.599@gmail.com>
2025-07-24 08:06:17 +0000

    Overhaul of the editing system plus support for nested paramters

--- simpsim/edits.scm
+++ simpsim/edits.scm
@@ -1,38 +1,100 @@
 (define-module (simpsim edits))
   
 
-(define-public c-style-param
-  (lambda (var)
-	(lambda (val)
-	  (lambda (file)
-		(format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$/\\1~a/g\" \"~a\"~%"
-				var val file)))))
+(define (c-replace file varname valstr)
+  (format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$/\\1~a/g\" \"~a\"~%"
+		  varname valstr file))
 
-(define-public (remove-param var)
-  (lambda (val)
-	(lambda (file)
-	  (format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$//g\" \"~a\"~%"
-			  var file))))
-
-(define-public (remove-when pred)
-  (lambda (var)
-	(lambda (val)
-	  (lambda (file)
-		(if (pred val)
-			(((remove-param var) val) file)
-			"")))))
+(define (c-remove file var)
+  (format #f "sed -i -e \"s/^\\(\\s*~a\\s*=\\s*\\)\\(.*\\)$//g\" \"~a\"~%"
+		  var file))
 				
+(define (c-insert-when-absent file var)
+  (format #f "grep \"^\\s*~a\\s*=\" \"~a\" > /dev/null || echo \"~a = \" >> \"~a\"~%"
+		  var file
+		  var file))
 
-(define-public (insert-when-absent var)
+(define (rev-range k)
+  (if (= k 0) '(0)
+	  (cons k (rev-range (1- k)))))
+
+(define (range k)
+  (reverse (rev-range (1- k))))
+
+
+(define-public (nested-c-replace-awk varchain val)
+  (format #f "BEGIN { insertlevel = ~a} { if (/{/) {FS = \"{\" ; node[k] = $1; gsub(/\\s/, \"\", node[k]) ; k++ }} {if (/}/) k--}\
+{if (k == insertlevel && ~a /^\\s*~a\\s*=/) {gsub(/=.*$/, \"= ~a\", $0)} ; print }"
+		  (1- (length varchain))
+		  (string-concatenate (map (lambda (x) (format #f "~a && " x))
+								   (map (lambda (k) (format #f "node[~a] == \"~a\""
+															k (list-ref varchain k)))
+										(range (1- (length varchain))))))
+		  (car (reverse varchain))
+		  val))
+
+
+(define-public (nested-c-search-awk varchain)
+  (format #f "BEGIN {present = 0 ; insertlevel = ~a ; FS = \"{\"} { if (/{/) {node[k] = $1; gsub(/\\s/, \"\", node[k]) ; k++ }} {if (/}/) k--}\
+{if (k == insertlevel && ~a /^\\s*~a\\s*=/) {present++}} END { if (present == 0) {exit 1} }"
+		  (1- (length varchain))
+		  (string-concatenate (map (lambda (x) (format #f "~a && " x))
+								   (map (lambda (k) (format #f "node[~a] == \"~a\""
+															k (list-ref varchain k)))
+										(range (1- (length varchain))))))
+		  (car (reverse varchain))))
+
+(define-public (nested-c-insert-awk varchain val)
+  (format #f "BEGIN {present = 0 ; insertlevel = ~a ; FS = \"{\"} { if (/{/) {node[k] = $1; gsub(/\\s/, \"\", node[k]) ; k++ }} {if (/}/) k--}\
+ { print } {if (k == insertlevel && ~a present == 0) { printf \"~a = ~a\\n\" ; present++}} END { if (present == 0) {exit 1} }"
+		  (1- (length varchain))
+		  (string-concatenate (map (lambda (x) (format #f "~a && " x))
+								   (map (lambda (k) (format #f "node[~a] == \"~a\""
+															k (list-ref varchain k)))
+										(range (1- (length varchain))))))
+		  (car (reverse varchain))
+		  val))
+
+(define-public (nested-c-comment-awk varchain)
+  (format #f "BEGIN { insertlevel = ~a} { if (/{/) {FS = \"{\" ; node[k] = $1; gsub(/\\s/, \"\", node[k]) ; k++ }} {if (/}/) k--}\
+{if (k == insertlevel && ~a /^\\s*~a\\s*=/) {printf \"// %s\\n\" $0} else print }"
+		  (1- (length varchain))
+		  (string-concatenate (map (lambda (x) (format #f "~a && " x))
+								   (map (lambda (k) (format #f "node[~a] == \"~a\""
+															k (list-ref varchain k)))
+										(range (1- (length varchain))))))
+		  (car (reverse varchain))))
+
+
+(define (nested-c-search file varchain)
+  (format #f "awk '~a' ~a" (nested-c-search-awk varchain) file))
+
+(define (nested-c-replace file varchain val)
+  (format #f "awk '~a' ~a" (nested-c-replace-awk varchain val) file))
+
+(define (nested-c-comment file varchain)
+  (format #f "awk '~a' ~a" (nested-c-comment-awk varchain) file))
+
+(define (nested-c-insert file varchain val)
+  (format #f "awk '~a' ~a" (nested-c-insert-awk varchain val) file))
+
+(define-public (generic-c varname val-format)
   (lambda (val)
-	(lambda (file)
-	  (format #f "grep \"^\\s*~a\\s*=\" \"~a\" > /dev/null || echo \"~a = ~a\" >> \"~a\"~%"
-			  var file
-			  var val file))))
-
-(define-public (edit-compose . fs)
-  (lambda (var)
-	(lambda (val)
+	(let ((valstr (val-format val)))
 	  (lambda (file)
-		(string-concatenate (map (lambda (f) (((f var) val) file))
-								 fs))))))
+		(if (eq? val 'none) (c-remove file varname)
+			(string-concatenate (list (c-insert-when-absent file varname)
+									  (c-replace file varname valstr))))))))
+  
+  
+(define-public (nested-c varchain val-format)
+  (lambda (val)
+	(let ((valstr (val-format val)))
+	(lambda (file)
+	  (if (eq? val 'none) (nested-c-comment file varchain)
+	   	  (format #f "(~a && ~a || ~a) > /tmp/paredit && mv /tmp/paredit ~a"
+				  (nested-c-search file varchain)
+				  (nested-c-replace file varchain valstr)
+				  (nested-c-insert file varchain valstr)
+				  file))))))
+
--- simpsim/formats.scm
+++ simpsim/formats.scm
@@ -7,11 +7,20 @@
 	(f (let ((comps (string-split (format #f "~,16,2e" x) (lambda (c) (char=? c #\E)))))
 		 (string-concatenate (list (car comps) "d" (cadr comps)))))))
 
-(define-public (c-double-format f)
+(define-public c-double-format
   (lambda (x)
-	(f (format #f "~,16,2e" x))))
+	(format #f "~,16,2e" x)))
 	
 
-(define-public (int-format f)
+(define-public int-format 
   (lambda (x)
-    (f (format #f "~d" (inexact->exact (ceiling x))))))
+    (format #f "~d" (inexact->exact (ceiling x)))))
+
+(define-public raw-format
+  (lambda (x) x))
+
+(define-public fortran-double-format
+  (lambda (x)
+	(string-map (lambda (c) (if (char=? c #\e) #\d
+								c))
+				(c-double-format x))))
